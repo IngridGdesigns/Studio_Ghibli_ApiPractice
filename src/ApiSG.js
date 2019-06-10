8dae07eac7f705b7742bc802581fcc0954b56124
Ingrid G <aleja.morada@gmail.com>
2019-06-10 15:38:49 -0700

    Modified css with stylesheet and react-bootstrap and cards

--- src/ApiSG.js
+++ src/ApiSG.js
@@ -1,41 +1,49 @@
-import React, {Component} from 'react'
-import Card from 'react-bootstrap/Card'
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
 
-class ApiSG extends Component{
-    constructor(props){
-        super(props);
+class ApiSG extends Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            films: [],
-        }
-    }
+    this.state = {
+      films: []
+    };
+  }
 
-    componentDidMount() {
-        fetch('https://ghibliapi.herokuapp.com/films', {
-            method: 'GET',
-        })   
-        .then(res => res.json()) //parsed
-        .then(data => this.setState({ films: data }))
-    }
-   
-   render() {
-       
-       console.log(this.state.films)
-       return(
-           
-           <div className='App'>
-                {this.state.loading || !this.state.films ? <h4>hello Api</h4> : 
-               this.state.films.map((movie ) => 
-               <div className='row'  key={movie.id}>
-                   <div className='column'>
-                   <h2>{movie.title}</h2>
-                   <p>{movie.description}</p>
-                   </div>
-               </div>)}
-              <h1>hello  now</h1>
-           </div>
-       )
-   }
+  componentDidMount() {
+    fetch("https://ghibliapi.herokuapp.com/films", {
+      method: "GET"
+    })
+      .then(res => res.json()) //parsed
+      .then(data => this.setState({ films: data }));
+  }
+
+  render() {
+    console.log(this.state.films);
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          {this.state.loading || !this.state.films ? (
+            <h4>Loading Studio ghibli Api</h4>
+          ) : (
+            this.state.films.map(movie => (
+              <Card
+                border="success"
+                key={movie.id}
+                className="col-md-6 col-lg-4"
+              >
+                <h1>{movie.title}</h1>
+                <Card.Body>
+                  <p>{movie.description}</p>
+                </Card.Body>
+              </Card>
+            ))
+          )}
+          <h3>loading...</h3>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default ApiSG
+export default ApiSG;
--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-import ApiSG from './ApiSG';
+import React from "react";
+import "./App.css";
+import ApiSG from "./ApiSG";
 
 function App() {
   return (
-    <div className='container'>
-      <h2>welcome</h2>
+    <div className="container">
+      <h2>Welcome to studio ghibli API practice</h2>
       <ApiSG />
     </div>
   );
